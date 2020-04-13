065bb482535c359609b3c3e7e4f99e89f6409825
Berenger <berenger.benoit@gmail.com>
2020-04-13 10:27:29 +0200

    registration form

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -34,11 +34,5 @@ class SecurityController extends AbstractController
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
 
-    /**
-     * @Route("/account", name="new_account")
-     */
-    public function newAccount()
-    {
-        
-    }
+  
 }
