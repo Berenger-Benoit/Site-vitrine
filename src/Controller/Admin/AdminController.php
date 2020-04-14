24ace8af3f8d6b34a6073e40f96184cc1c39fc26
Berenger <berenger.benoit@gmail.com>
2020-04-14 15:36:52 +0200

    add form new product

--- src/Controller/Admin/AdminController.php
+++ src/Controller/Admin/AdminController.php
@@ -2,8 +2,11 @@
 
 namespace App\Controller\Admin;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Product;
+use App\Form\ProductType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class AdminController extends AbstractController
 {
@@ -12,8 +15,35 @@ class AdminController extends AbstractController
      */
     public function index()
     {
-        return $this->render('admin/admin/index.html.twig', [
+        return $this->render('admin/index.html.twig', [
             'controller_name' => 'AdminController',
         ]);
     }
+
+        /**
+     * @Route("/admin/new", name="admin_new")
+     */
+    public function new(Request $request)
+    {
+        $product = new Product();
+        $form = $this->createForm(ProductType::class, $product);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+
+            $em->persist($product);
+
+            $em->flush();
+
+            $this->addFlash('success', 'Le bien a été ajouté!');
+
+            return $this->redirectToRoute('home');
+        }
+
+        return $this->render('admin/new.html.twig', [
+            'form' => $form->createView(),
+            ]);
+    }
+
 }
--- src/Form/ProductType.php
+++ src/Form/ProductType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Entity\Product;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,12 +14,16 @@ class ProductType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name')
+            ->add('name', TextType::class, [
+                'required' => true,
+            ])
             ->add('description')
             ->add('origin')
             ->add('price')
             ->add('weight')
-            ->add('category')
+            ->add('category',ChoiceType::class, [
+                'required' => true,
+            ])
         ;
     }
 
